981e74f1a4349548fff01203010711e4e3e49705
rubinghimire <rubin.ghimire@gmail.com>
2023-09-26 14:02:57 +0545

    added user input feature and fixed responsiveness

--- app.js
+++ app.js
@@ -1,24 +1,43 @@
-const song = document.querySelector(".song"); //Audio element
-const play = document.querySelector(".play"); //Play button
-const replay = document.querySelector(".replay"); //Replay button
-const outline = document.querySelector(".moving-outline circle"); //Circle element
-const video = document.querySelector(".vid-container video"); //Video element
-//Sounds
+// Audio element
+const song = document.querySelector(".song"); 
+// Play button
+const play = document.querySelector(".play"); 
+// Replay button
+const replay = document.querySelector(".replay"); 
+// Circle element
+const outline = document.querySelector(".moving-outline circle"); 
+// Video element
+const video = document.querySelector(".vid-container video");
+// Sounds
 const sounds = document.querySelectorAll(".sound-picker button");
-//Time Display
+// Time Display
 const timeDisplay = document.querySelector(".time-display");
-//Calculate outline length
+// Calculate outline length
 const outlineLength = outline.getTotalLength();
-//Duration
+// Duration
+// Get the input element
+let timeInput = document.querySelector(".time-select input");
+// Get buttons
 const timeSelect = document.querySelectorAll(".time-select button");
 let fakeDuration = 300;
 
-//Initialize outline properties
+// Initialize outline properties
 outline.style.strokeDashoffset = outlineLength;
 outline.style.strokeDasharray = outlineLength;
 
-//Function to format time with two digits
-//Using toLocaleString to get two digits for seconds
+// Function to run when the page is loaded
+window.addEventListener("load", function() {
+  // Get the time input element
+  const timeInput = document.querySelector(".time-select input");
+  // Set the input field's value to an empty string
+  timeInput.value = "";
+  // Set focus on the input field
+  timeInput.focus();
+});
+
+
+// Function to format time with two digits
+// Using toLocaleString to get two digits for seconds
 function formatTime(timeInSeconds) {
   const minutes = Math.floor(timeInSeconds / 60);
   const seconds = Math.floor(timeInSeconds % 60).toLocaleString('en-US', {
@@ -27,43 +46,7 @@ function formatTime(timeInSeconds) {
   return `${minutes}:${seconds}`;
 }
 
-//Initialize time display
-timeDisplay.textContent = formatTime(fakeDuration);
-
-//Event listener for sound picker buttons
-sounds.forEach(sound => {
-  sound.addEventListener("click", function() {
-    song.src = this.getAttribute("data-sound");
-    video.src = this.getAttribute("data-video");
-    checkPlaying(song);
-  });
-});
-
-//Event listener for play button
-play.addEventListener("click", function() {
-  checkPlaying(song);
-});
-
-//Event listener for replay button
-replay.addEventListener("click", function() {
-    restartSong(song);
-    timeDisplay.textContent = formatTime(fakeDuration);
-});
-
-//Function to restart the song
-const restartSong = song =>{
-    song.currentTime = 0;
-}
-
-//Event listener for time select buttons
-timeSelect.forEach(option => {
-  option.addEventListener("click", function() {
-    fakeDuration = this.getAttribute("data-time");
-    timeDisplay.textContent = formatTime(fakeDuration);
-  });
-});
-
-//Function to play or pause the audio and video
+// Function to play or pause the audio and video
 const checkPlaying = song => {
   if (song.paused) {
     song.play();
@@ -76,7 +59,62 @@ const checkPlaying = song => {
   }
 };
 
-//Update time display and progress bar
+// Function to restart the song
+const restartSong = song =>{
+  song.currentTime = 0;
+}
+
+// Initialize time display
+timeDisplay.textContent = formatTime(fakeDuration);
+
+// Event listener for sound picker buttons
+sounds.forEach(sound => {
+  sound.addEventListener("click", function() {
+    song.src = this.getAttribute("data-sound");
+    video.src = this.getAttribute("data-video");
+    checkPlaying(song);
+  });
+});
+
+// Event listener for play button
+play.addEventListener("click", function() {
+  checkPlaying(song);
+});
+
+// Event listener for replay button
+replay.addEventListener("click", function() {
+    restartSong(song);
+    timeDisplay.textContent = formatTime(fakeDuration);
+});
+
+
+// Event listener for time select input change event
+timeInput.addEventListener("keydown", function(event) {
+  if(event.key === "Enter") {
+    // Change the 'data-time' attribute to the current value of the input box
+    this.setAttribute("data-time", this.value);
+    fakeDuration = (this.getAttribute("data-time")) * 60; //This value is in minutes
+    timeDisplay.textContent = formatTime(fakeDuration);
+    // Change the width of the input field
+    // this.style.width = "30%";
+    this.classList.add("input-expanded");
+    // Add class since input field is filled now
+    this.classList.add("filled"); 
+  }
+});
+
+// Event listener for time select buttons
+timeSelect.forEach(option => {
+  // Check if the option is a button
+  if (option.tagName === "BUTTON") {
+    option.addEventListener("click", function() {
+    fakeDuration = this.getAttribute("data-time");
+    timeDisplay.textContent = formatTime(fakeDuration);
+    });
+  }
+});
+
+// Update time display and progress bar
 song.ontimeupdate = function() {
   let currentTime = song.currentTime;
   let elapsed = fakeDuration - currentTime;
